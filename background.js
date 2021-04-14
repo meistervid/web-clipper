96ce13cdfb4fea820a2a41cd7081361ecb9bf998
Vid <vid@meisterlabs.com>
2021-04-14 20:02:16 +0200

    testing fetching

--- background.js
+++ background.js
@@ -1,6 +1,43 @@
-let type = 'content';
+let color = '#3aa757';
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ type });
-  console.log('Yolo wololo');
+  chrome.storage.sync.set({ color });
+  console.log('Default background color set to %cgreen', `color: ${color}`);
+  // fetch('http://localhost:5001/mobile/login/1')
+  //     .then(response => response.text())
+  //     .then(function(token) {
+  //       console.log('token', token)
+  //         return fetch('http://localhost:5001/mobile/api/gql', {
+  //             body: JSON.stringify({ query: 'query {  foldersAndNotesByStatus(folderStatus:ACTIVE, noteStatus:ACTIVE) {    name,    id  }}', variables: {} }),
+  //             method: 'POST',
+  //             headers: {
+  //               Authorization: `Bearer ${token}`,
+  //               'Content-Type': 'application/json'
+  //             },
+  //         });
+  //     })
+  //     // .then(response => sendResponse({result: response.results}))
+  //     .then(response => response.json())
+  //     .then(console.log);
 });
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    fetch('http://localhost:5001/mobile/login/1')
+      .then(response => response.text())
+      .then(function(token) {
+        console.log('token', token)
+          return fetch('http://localhost:5001/mobile/api/gql', {
+              body: JSON.stringify({ query: 'query {  foldersAndNotesByStatus(folderStatus:ACTIVE, noteStatus:ACTIVE) {    name,    id  }}', variables: {} }),
+              method: 'POST',
+              headers: {
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json'
+              },
+          });
+      })
+      .then(response => sendResponse({result: response.results}))
+      // .then(response => response.json())
+      // .then(console.log);
+      return true;
+  });
