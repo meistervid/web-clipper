177d8565c1bb1045919fe45c9bb73a4c4226cef2
Vid <vid@meisterlabs.com>
2021-04-15 13:06:38 +0200

    cleanup

--- background.js
+++ background.js
@@ -1,43 +1,20 @@
 let color = '#3aa757';
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-  // fetch('http://localhost:5001/mobile/login/1')
-  //     .then(response => response.text())
-  //     .then(function(token) {
-  //       console.log('token', token)
-  //         return fetch('http://localhost:5001/mobile/api/gql', {
-  //             body: JSON.stringify({ query: 'query {  foldersAndNotesByStatus(folderStatus:ACTIVE, noteStatus:ACTIVE) {    name,    id  }}', variables: {} }),
-  //             method: 'POST',
-  //             headers: {
-  //               Authorization: `Bearer ${token}`,
-  //               'Content-Type': 'application/json'
-  //             },
-  //         });
-  //     })
-  //     // .then(response => sendResponse({result: response.results}))
-  //     .then(response => response.json())
-  //     .then(console.log);
-});
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    fetch('http://localhost:5001/mobile/login/1')
-      .then(response => response.text())
-      .then(function(token) {
-        console.log('token', token)
-          return fetch('http://localhost:5001/mobile/api/gql', {
-              body: JSON.stringify({ query: 'query {  foldersAndNotesByStatus(folderStatus:ACTIVE, noteStatus:ACTIVE) {    name,    id  }}', variables: {} }),
-              method: 'POST',
-              headers: {
-                Authorization: `Bearer ${token}`,
-                'Content-Type': 'application/json'
-              },
-          });
-      })
-      .then(response => sendResponse({result: response.results}))
-      // .then(response => response.json())
-      // .then(console.log);
-      return true;
-  });
+// chrome.runtime.onInstalled.addListener(() => {
+//   chrome.storage.sync.set({ color });
+//   fetch('http://localhost:5001/mobile/login/1')
+//         .then(response => response.text())
+//         .then(function(token) {
+//           console.log('token', token)
+//             return fetch('http://localhost:5001/mobile/api/gql', {
+//                 body: JSON.stringify({ query: 'query {  foldersAndNotesByStatus(folderStatus:ACTIVE, noteStatus:ACTIVE) {    name,    id  }}', variables: {} }),
+//                 method: 'POST',
+//                 headers: {
+//                   Authorization: `Bearer ${token}`,
+//                   'Content-Type': 'application/json'
+//                 },
+//             });
+//         })
+//         .then(response => response.json())
+//         .then(console.log);
+// });
--- popup.js
+++ popup.js
@@ -1,48 +1,22 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
+let fetchWorkspaces = document.getElementById("fetchWorkspaces");
 
-chrome.storage.sync.get("color", ({ color }) => {
-  console.log('loaded color no action whatever shit piss', color)
-  changeColor.style.backgroundColor = color;
+fetchWorkspaces.addEventListener("click", async () => {
+  console.log('lololol')
+  fetch('http://localhost:5001/mobile/login/1')
+    .then(response => response.text())
+    .then(function(token) {
+      console.log('token', token)
+        return fetch('http://localhost:5001/mobile/api/gql', {
+            body: JSON.stringify({ query: 'query {  foldersAndNotesByStatus(folderStatus:ACTIVE, noteStatus:ACTIVE) {    name,    id  }}', variables: {} }),
+            method: 'POST',
+            headers: {
+              Authorization: `Bearer ${token}`,
+              'Content-Type': 'application/json'
+            },
+        });
+    })
+    .then(response => response.json())
+    .then(({ data }) => {
+      document.getElementById("fetchWorkspaces").innerHTML = JSON.stringify(data);
+    });
 });
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-    console.log('lololol')
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
-  });
-  
-  // The body of this function will be executed as a content script inside the
-  // current page
-  async function setPageBackgroundColor() {
-
-
-    const data = await fetch('http://localhost:5001/mobile/login/1')
-      .then(response => response.text())
-      .then(function(token) {
-        console.log('token', token)
-          return fetch('http://localhost:5001/mobile/api/gql', {
-              body: JSON.stringify({ query: 'query {  foldersAndNotesByStatus(folderStatus:ACTIVE, noteStatus:ACTIVE) {    name,    id  }}', variables: {} }),
-              method: 'POST',
-              headers: {
-                Authorization: `Bearer ${token}`,
-                'Content-Type': 'application/json'
-              },
-          });
-      })
-      .then(response => response.json())
-      .then(console.log);
-      
-    console.log(data)
-
-    chrome.storage.sync.get("color", ({ color }) => {
-      console.log('color', color)
-      document.body.style.backgroundColor = color;
-    });
-}
