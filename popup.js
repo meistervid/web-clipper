c2163168d4475614d607b7e745b6193c0b8b8730
Vid <vid@meisterlabs.com>
2021-04-16 11:11:16 +0200

    open screenshot in new tab fullscreen

--- popup.js
+++ popup.js
@@ -1,11 +1,9 @@
 let fetchWorkspaces = document.getElementById("fetchWorkspaces");
 
 fetchWorkspaces.addEventListener("click", async () => {
-  console.log('lololol')
   fetch('http://localhost:5001/mobile/login/1')
     .then(response => response.text())
     .then(function(token) {
-      console.log('token', token)
         return fetch('http://localhost:5001/mobile/api/gql', {
             body: JSON.stringify({ query: 'query {  foldersAndNotesByStatus(folderStatus:ACTIVE, noteStatus:ACTIVE) {    name,    id  }}', variables: {} }),
             method: 'POST',
@@ -21,118 +19,10 @@ fetchWorkspaces.addEventListener("click", async () => {
     });
 
     chrome.tabs.captureVisibleTab(null,{},function(dataUrl){
-      const screenshot = document.getElementById("screenshot");
-      document.getElementById("screenshot").src = dataUrl;
-
-      console.log('screenshot', screenshot.clientWidth)
-
-      const canvas = document.createElement('canvas');
-
-      canvas.id = "canvas";
-      canvas.width = 600;
-      canvas.height = 600;
-      canvas.style.top = '0px';
-      canvas.style.position = "absolute";
-      canvas.style.zIndex = 999;
-
-      const body = document.getElementsByTagName("body")[0];
-      body.appendChild(canvas);
-
-      // Some optional drawings.
-      var ctx = canvas.getContext("2d");
-      console.log('ctx', ctx)
-
-      // style the context
-      ctx.strokeStyle = "blue";
-      ctx.lineWidth = 3;
-
-      // calculate where the canvas is on the window
-      // (used to help calculate mouseX/mouseY)
-      var offsetX = 8;
-      var offsetY = 0;
-
-      // this flage is true when the user is dragging the mouse
-      var isDown = false;
-
-      // these vars will hold the starting mouse position
-      var startX;
-      var startY;
-
-
-      function handleMouseDown(e) {
-          e.preventDefault();
-          e.stopPropagation();
-
-          // save the starting x/y of the rectangle
-          startX = parseInt(e.clientX - offsetX);
-          startY = parseInt(e.clientY - offsetY);
-
-          // set a flag indicating the drag has begun
-          isDown = true;
-      }
-
-      function handleMouseUp(e) {
-          e.preventDefault();
-          e.stopPropagation();
-
-          // the drag is over, clear the dragging flag
-          isDown = false;
-      }
-
-      function handleMouseOut(e) {
-          e.preventDefault();
-          e.stopPropagation();
-
-          // the drag is over, clear the dragging flag
-          isDown = false;
-      }
-
-      function handleMouseMove(e) {
-          e.preventDefault();
-          e.stopPropagation();
-
-          // if we're not dragging, just return
-          if (!isDown) {
-              return;
-          }
-
-          // get the current mouse position
-          mouseX = parseInt(e.clientX - offsetX);
-          mouseY = parseInt(e.clientY - offsetY);
-
-          // Put your mousemove stuff here
-
-          // clear the canvas
-          ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-          // calculate the rectangle width/height based
-          // on starting vs current mouse position
-          var width = mouseX - startX;
-          var height = mouseY - startY;
-
-          // draw a new rect from the start position 
-          // to the current mouse position
-          ctx.strokeRect(startX, startY, width, height);
-
-      }
-
-      // listen for mouse events
-      canvas.onmousedown = function (e) {
-        console.log('e', e)
-          handleMouseDown(e);
-      };
-      canvas.onmousemove = function (e) {
-        console.log('e', e)
-          handleMouseMove(e);
-      };
-      canvas.onmouseup = function (e) {
-        console.log('e', e)
-          handleMouseUp(e);
-      };
-      // canvas.onmouseout = function (e) {
-      //   console.log('e', e)
-      //     handleMouseOut(e);
-      // };
+      localStorage.clear();
+      const id = Date.now();
+      localStorage.setItem(id, dataUrl);
+      window.open(`chrome-extension://kciheegmooocpbodmemaongdaohpfbdd/screenshot.html?id=${id}`);
     });
 });
 
---
+++ screenshot.js
@@ -0,0 +1,105 @@
+const queryString = location.search;
+const urlParams = new URLSearchParams(queryString);
+const idpassed = urlParams.get('id')
+
+const body = window.document.getElementsByTagName("body")[0];
+body.style.margin = 0;
+
+// set up screenshot
+const img = new Image();
+img.src = localStorage.getItem(idpassed);
+img.style.width = '100%';
+img.style.border = 'none';
+body.appendChild(img);
+
+// set up canvas
+const canvas = window.document.createElement('canvas');
+canvas.id = "canvas";
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.style.top = '0px';
+canvas.style.left = '0px';
+canvas.style.position = "absolute";
+canvas.style.zIndex = 999;
+body.appendChild(canvas);
+
+// Some optional drawings.
+var ctx = canvas.getContext("2d");
+
+// style the context
+ctx.strokeStyle = "black";
+ctx.setLineDash([2, 5]);
+ctx.lineWidth = 2;
+
+// calculate where the canvas is on the window
+// (used to help calculate mouseX/mouseY)
+var offsetX = 8;
+var offsetY = 0;
+
+// this flage is true when the user is dragging the mouse
+var isDown = false;
+
+// these vars will hold the starting mouse position
+var startX;
+var startY;
+
+
+function handleMouseDown(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    // save the starting x/y of the rectangle
+    startX = parseInt(e.clientX - offsetX);
+    startY = parseInt(e.clientY - offsetY);
+
+    // set a flag indicating the drag has begun
+    isDown = true;
+}
+
+function handleMouseUp(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    // the drag is over, clear the dragging flag
+    isDown = false;
+}
+
+function handleMouseMove(e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    // if we're not dragging, just return
+    if (!isDown) {
+        return;
+    }
+
+    // get the current mouse position
+    mouseX = parseInt(e.clientX - offsetX);
+    mouseY = parseInt(e.clientY - offsetY);
+
+    // Put your mousemove stuff here
+
+    // clear the canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // calculate the rectangle width/height based
+    // on starting vs current mouse position
+    var width = mouseX - startX;
+    var height = mouseY - startY;
+
+    // draw a new rect from the start position 
+    // to the current mouse position
+    ctx.strokeRect(startX, startY, width, height);
+
+}
+
+// listen for mouse events
+canvas.onmousedown = function (e) {
+    handleMouseDown(e);
+};
+canvas.onmousemove = function (e) {
+    handleMouseMove(e);
+};
+canvas.onmouseup = function (e) {
+    handleMouseUp(e);
+};
